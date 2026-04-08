bbc22fe856a09f54a40fb9b97a45e88fade5f328
Raja Patnaik <patnaik.raja@gmail.com>
2026-04-08 14:09:22 -0700

    fix clipboard passthrough and markdown nvim extension issue

--- editor/nvim/.config/nvim/lua/plugins/init.lua
+++ editor/nvim/.config/nvim/lua/plugins/init.lua
@@ -125,7 +125,7 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = "cd app && npm install",
+    build = "cd app && npm install && git checkout -- yarn.lock 2>/dev/null || true",
     keys = {
       { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
     },
